caa8f095a99b701c2c8bbfe9e71b7bf633ece979
70mm1 <mail@tmirwald.de>
2022-10-26 01:17:20 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Triggerrrrr.
+          Edit <code>src/App.js</code> and save to reload BANG.
         </p>
         <a
           className="App-link"
