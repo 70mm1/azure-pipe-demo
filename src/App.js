c035162dfac0db6206ec47e785784e0b3daa254c
70mm1 <mail@tmirwald.de>
2022-10-25 21:40:02 +0200

    Website Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { Link } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
- return (
- <div>
- <Link to="/about">About</Link>
- <h2>Github Pages</h2>
- <h3>Deploying React to Github</h3>
- </div>
- );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
