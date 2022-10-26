8e6e0fcde6b720c590daa997c3de9087435dc791
70mm1 <mail@tmirwald.de>
2022-10-26 14:31:55 +0200

    Wuhuuuuuuu

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Asteroid Bang Bang Bang.
+          DAT Asteroid Bang Bang.
         </p>
         <a
           className="App-link"
